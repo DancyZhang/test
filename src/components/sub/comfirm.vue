26311543651624916e28a31bf250fce0edefc71b
Dancy <zhangfengming_v@didichuxing.con>
2019-07-29 15:26:59 +0800

    start

---
+++ src/components/sub/comfirm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="comfirm">
+        <div>{{ msg }}</div>
+        <button @click="getButton">{{text}}</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"comfirm",
+    props:["text"],
+    data() {
+        return {
+            msg:'say hello'
+        }
+    },
+    methods: {
+        getButton(){
+            this.$emit('message',this.msg)  //点击传值给父组件
+        },
+    },
+}
+</script>
---
+++ src/components/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="home">
+       <span>{{ msg }}</span> 
+       comfirm
+    </div>
+</template>
+
+<script>
+export default {
+    name:'home',
+    data() {
+        return {
+            msg:'开始我们的vue项目'
+        }
+    },
+}
+</script>
+<style lang="scss" type="text/css" scoped>
+    span{
+        font-size:30px;
+        color: aquamarine;
+    }
+</style>
